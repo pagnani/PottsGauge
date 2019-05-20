93acfcb821cf4eedafb37a344113d04196c2bd29
Andrea Pagnani <andrea.pagnani@gmail.com>
2019-05-20 08:12:54 +0200

    fix L->N in help

--- src/PottsGauge.jl
+++ src/PottsGauge.jl
@@ -31,7 +31,7 @@ Usage:
     gauge(J,h,ExternalGauge(U,V,C))
 where:
     * For `WildType` `x0` is a `Vector{Int}` of size `N` whose values are in the interval `1,…,q`
-    * For `ExternalGauge`, `U`,`V` are `q×L×L` arrays, and C is vector of length `L`.
+    * For `ExternalGauge`, `U`,`V` are `q×N×N` arrays, and C is vector of length `N`.
 """
 function gauge(J::Array{F,4},h::Array{F,2},gauge::T) where F <: Real where T <: Gauge
 
